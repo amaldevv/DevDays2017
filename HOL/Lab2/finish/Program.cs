16ece435a888ff4e6acfec8f2de40e8d92dcd73f
Amal <amaldevv@outlook.com>
2017-02-20 04:17:21 +0000

    Added Lab 2 - Converting Console app to web app

---
+++ HOL/Lab2/finish/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+namespace finish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(context => context.Response.WriteAsync("Hello World!")))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
